07e8dd1f0ae45a3464149cfa83b43f1d48261640
Akhil Ashokan <akhil.ashokan@litmus7.com>
2025-12-15 10:56:36 +0530

    refactor: add repo name

--- lib/prismic.ts
+++ lib/prismic.ts
@@ -1,6 +1,7 @@
 import * as prismic from '@prismicio/client';
 
-const endpoint = 'https://<your-repo-name>.prismic.io/api/v2';
+const repoName = process.env.NEXT_PUBLIC_REPO_NAME || ''
+const endpoint = `https://${repoName}.prismic.io/api/v2`;
 const client = prismic.createClient(endpoint);
 
 export default client;
