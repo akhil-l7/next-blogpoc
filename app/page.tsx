4f5b60e75b22f562919fc64fd8926d0fcaa62b19
akhill7 <akhil.ashokan@litmus7.com>
2025-12-24 12:54:51 +0530

    refactor: add output export, remove force-dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 import client from "@/lib/prismic";
 
-export const dynamic = 'force-dynamic';
-
 export default async function Home() {
   const data = await client.getAllByType('blog');
   return (
