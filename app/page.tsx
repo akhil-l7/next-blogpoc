5a4a4aeebb2bb6fffd1d732cbb134b006cffafa0
akhill7 <akhil.ashokan@litmus7.com>
2026-01-09 15:55:15 +0530

    refactor: update cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,39 @@
-import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { config } from "@/app.config";
+import { Card, CardAction, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
 import client from "@/lib/prismic";
+import { ChevronRightIcon } from "lucide-react";
 import Link from "next/link";
-import { config } from "@/app.config"
 
 export default async function Home() {
   const data = await client.getAllByType('blog');
   return (
-    <main className="max-w-7xl px-2 mx-auto">
+    <main className="container px-2 mx-auto">
       <header className="my-10">
         <h1 className="scroll-m-20 text-center text-4xl tracking-tight text-balance">{config?.title || 'blog'}</h1>
       </header>
       <Separator className="mb-6" />
-      <section className="">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+      <section>
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-8">
           {
             data?.map(({ data, id }) => (
               <Card key={id}>
-                <Link href={`/${id}`}>
-                  <CardHeader>
-                    <CardTitle>
-                      {data?.title}
-                    </CardTitle>
-                  </CardHeader>
-                </Link>
+                <CardHeader>
+                  <CardTitle>
+                    {data?.title || 'Title'}
+                  </CardTitle>
+                  <CardDescription>
+                    {data?.description || 'Description'}
+                  </CardDescription>
+                  <CardAction>
+                    <Link href={`/${id}`}>
+                      <ChevronRightIcon />
+                    </Link>
+                  </CardAction>
+                </CardHeader>
+                <CardContent>
+                  {data?.description || 'Content'}
+                </CardContent>
               </Card>
             ))
           }
