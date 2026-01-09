9547102bc6ca561cf7c9c1c43895cf5ea33701f1
akhill7 <akhil.ashokan@litmus7.com>
2026-01-09 13:24:27 +0530

    feat: add config

---
+++ app.config.ts
@@ -0,0 +1,4 @@
+export const config: any = {
+    title: 'Blog POC',
+    description: 'SSG blog hosted on github pages'
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,14 @@ import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
 import client from "@/lib/prismic";
 import Link from "next/link";
+import { config } from "@/app.config"
 
 export default async function Home() {
   const data = await client.getAllByType('blog');
   return (
     <main className="max-w-7xl px-2 mx-auto">
       <header className="my-10">
-        <h1 className="scroll-m-20 text-center text-4xl tracking-tight text-balance">POC Blog</h1>
+        <h1 className="scroll-m-20 text-center text-4xl tracking-tight text-balance">{config?.title || 'blog'}</h1>
       </header>
       <Separator className="mb-6" />
       <section className="">
