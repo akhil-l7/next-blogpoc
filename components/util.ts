207be92bfa48bae9736e85bb7a9d5e0de260fefa
akhill7 <akhil.ashokan@litmus7.com>
2026-02-06 15:15:32 +0530

    feat: getReadTime && getWordCount

--- components/util.ts
+++ components/util.ts
@@ -1,3 +1,19 @@
 export function sleep(ms: number): Promise<void> {
     return new Promise((resolve) => setTimeout(resolve, ms));
 }
+
+export function getReadTime(text: string, wordsPerMin: number = 200) {
+    const wordCount = getWordCount(text);
+    const minutes = Math.ceil(wordCount / wordsPerMin);
+    return minutes;
+}
+
+function getWordCount(text: string) {
+    const trimmedText = text.trim();
+ 
+    if (trimmedText === "") {
+        return 0;
+    }
+    const words = trimmedText.split(/\s+/);
+    return words.length;
+}
