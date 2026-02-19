a110992408dad676109bfaf6e0b6fb2709b6b08e
akhill7 <akhil.ashokan@litmus7.com>
2026-02-19 16:18:53 +0530

    refactor: remove endpoint, use reponame

--- lib/prismic.ts
+++ lib/prismic.ts
@@ -1,7 +1,7 @@
 import * as prismic from '@prismicio/client';
 
 const repoName = process.env.NEXT_PUBLIC_REPO_NAME || ''
-const endpoint = `https://${repoName}.prismic.io/api/v2`;
-const client = prismic.createClient(endpoint);
+// const endpoint = `https://${repoName}.prismic.io/api/v2`;
+const client = prismic.createClient(repoName);
 
 export default client;
