784cfbcf44ef0a0b86fa54dd80e148d5fcf61699
akhill7 <akhil.ashokan@litmus7.com>
2026-02-20 16:31:48 +0530

    feat: add Config type

--- app.config.ts
+++ app.config.ts
@@ -1,4 +1,8 @@
-export const config: any = {
+interface Config {
+    title: string;
+    description: string;
+}
+export const config: Config = {
     title: 'Blog POC',
     description: 'SSG blog hosted on github pages'
 }
