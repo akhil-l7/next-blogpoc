0497b98a075a404f3b2b689197831d06a62fbb9b
akhill7 <akhil.ashokan@litmus7.com>
2026-02-20 20:46:17 +0530

    refactor: update header with ColorSchemaToggle

--- components/header.tsx
+++ components/header.tsx
@@ -1,4 +1,5 @@
 import { KeyTextField } from "@prismicio/client";
+import ColorSchemaToggle from "./ui/colorSchemaToggle";
 
 interface HeaderProps {
     title?: KeyTextField;
@@ -7,8 +8,9 @@ interface HeaderProps {
 export const Header = ({ title = 'blog' }: HeaderProps) => {
 
     return (
-        <header className="my-2 md:my-8">
+        <header className="my-2 md:my-8 flex justify-between px-2 lg:px-0 lg:justify-center lg:flex-col lg:gap-2 lg:mb-4">
             <h1 className="scroll-m-20 text-center text-4xl tracking-tight text-balance">{title}</h1>
+            <span className="flex justify-center"><ColorSchemaToggle /></span>
         </header>
     );
 };
