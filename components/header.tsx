c923f5b07f0e65eefd071009202fd9a04c0b82d3
akhill7 <akhil.ashokan@litmus7.com>
2026-02-23 15:47:59 +0530

    style: alignment fix

--- components/header.tsx
+++ components/header.tsx
@@ -8,7 +8,7 @@ interface HeaderProps {
 export const Header = ({ title = 'blog' }: HeaderProps) => {
 
     return (
-        <header className="main_header my-2 md:my-8 flex justify-between px-2 lg:px-0 lg:justify-center lg:flex-col lg:gap-2 lg:mb-4">
+        <header className="main_header my-2 md:my-8 flex justify-between items-center px-2 lg:px-0 lg:justify-center lg:flex-col lg:gap-2 lg:mb-4">
             <h1 className="text-center text-4xl tracking-tight text-balance">{title}</h1>
             <span className="flex justify-center"><ColorSchemaToggle /></span>
         </header>
