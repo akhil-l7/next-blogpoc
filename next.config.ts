0801f1629e21e941eda73be6cd9090793df9aa73
akhill7 <akhil.ashokan@litmus7.com>
2026-02-24 17:52:03 +0530

    refactor: config update for vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,6 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: isProd ? '/next-blogpoc' : undefined,
-  assetPrefix: isProd ? '/next-blogpoc' : undefined,
-  trailingSlash: true,
   experimental: {
     viewTransition: true
   }
