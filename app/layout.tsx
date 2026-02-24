e4078d9b198e4de13aad83d76f66eb1cdfea2805
akhill7 <akhil.ashokan@litmus7.com>
2026-02-24 20:21:58 +0530

    add rediect with meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,6 @@
-import { config } from "@/app.config";
-import { Separator } from "@/components/ui/separator";
-import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Header } from "@/components/header";
+import Head from "next/head";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -14,28 +11,7 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
-const isDev = process.env.NODE_ENV === 'development'
 
-const csp = `
-    default-src 'self';
-    script-src 'self' 'unsafe-inline'${isDev ? " 'unsafe-eval'" : ''};
-    style-src 'self' 'unsafe-inline';
-    img-src 'self' blob: data:;
-    font-src 'self';
-    object-src 'none';
-    base-uri 'self';
-    form-action 'self';
-    frame-ancestors 'none';
-    upgrade-insecure-requests;
-`.replace(/\s{2,}/g, ' ').trim();
-
-export const metadata: Metadata = {
-  title: config.title || 'Blog',
-  description: config.description || "Description of blog",
-  other: {
-    "Content-Security-Policy": csp
-  }
-};
 
 export default function RootLayout({
   children,
@@ -44,11 +20,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta key={'redirect'} httpEquiv="refresh" content="0;url=https://ak-blogpoc.vercel.app/" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased dark`}
       >
-        <Header title={config.title} />
-        <Separator className="" />
         <main className="container max-w-5xl px-2 mx-auto overflow-hidden">
           <div className="content_container">
             {children}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
+import { Separator } from "@/components/ui/separator";
+import { Globe } from "lucide-react";
 
 export default async function Home() {
   return (
-    <div className="flex flex-col justify-center items-center h-full ">
-      <h1 className="text-4xl">migrated to vercel</h1>
-      <p>
-        now at <a href="https://ak-blogpoc.vercel.app/">ak-blogpoc</a>
+    <div className="flex flex-col justify-center items-center h-dvh">
+      <h1 className="text-4xl">Migrated To Vercel</h1>
+      <Separator className="my-2" />
+      <p className="flex gap-2">
+        now at
+        <a className="flex gap-2 text-blue-700" href="https://ak-blogpoc.vercel.app/">ak-blogpoc <Globe /> </a>
       </p>
     </div>
   );
