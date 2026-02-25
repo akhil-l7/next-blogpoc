e88ec8dd3b4bd62c6cf764d1e5274e454ee8cdb1
akhill7 <akhil.ashokan@litmus7.com>
2026-02-25 16:50:50 +0530

    chore: enable Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components/header";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -54,6 +55,7 @@ export default function RootLayout({
             {children}
           </div>
         </main>
+        <SpeedInsights />
       </body>
     </html>
   );
