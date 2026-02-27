60977924a7cddad8686b80e5fca9fb3e935814d1
akhill7 <akhil.ashokan@litmus7.com>
2026-02-27 19:03:12 +0530

    fix: button accessibility

--- components/ui/colorSchemaToggle.tsx
+++ components/ui/colorSchemaToggle.tsx
@@ -6,6 +6,7 @@ import { sleep } from "../util";
 export default function ColorSchemaToggle() {
     const [isDark, setIsDark] = useState(true);
     const [isAnimating, setIsAnimating] = useState(false);
+    const label = isDark ? "Switch to light mode" : "Switch to dark mode";
 
     async function toggleSchema(): Promise<void> {
         if (!document) return
@@ -30,7 +31,7 @@ export default function ColorSchemaToggle() {
     }, []);
 
     return (
-        <button data-isanimating={isAnimating} className="px-2 py-0.5 rounded-sm lg:p-2 lg:rounded-4xl cursor-pointer relative data-[isAnimating=true]:pointer-events-none select-none hover:bg-toggle-focus" onClick={toggleSchema}>
+        <button title={label} aria-label={label} aria-pressed={isDark} data-isanimating={isAnimating} className="px-2 py-0.5 rounded-sm lg:p-2 lg:rounded-4xl cursor-pointer relative data-[isAnimating=true]:pointer-events-none select-none hover:bg-toggle-focus" onClick={toggleSchema}>
             <span className="block relative rounded-2xl w-6 h-6 overflow-hidden *:absolute *:origin-bottom *:transition-transform *:ease-in-out *:duration-1000 motion-reduce:*:duration-[0s]">
                 <Sun color="var(--toggle-sun)" data-isdark={isDark} className="rotate-0 data-[isDark=true]:rotate-180" />
                 <Moon color="var(--toggle-moon)" data-isdark={isDark} className="rotate-180 data-[isDark=true]:rotate-360" />
