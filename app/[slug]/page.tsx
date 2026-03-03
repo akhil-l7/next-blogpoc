3287f0930ed7a3a358df0452485be382c2f4359a
akhill7 <akhil.ashokan@litmus7.com>
2026-03-03 17:22:41 +0530

    refactor: remove redundant divs

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -18,25 +18,19 @@ export default async function blog({ params }: { params: Promise<{ slug: string 
     notFound();
   }
   return (
-    <div className="flex-1 container mx-auto">
-      <div className="flex items-center">
-        <div className="bg-background min-h-screen w-full">
-          <div className="mx-auto max-w-4xl p-1 md:p-1 lg:px-6">
-            {/* https://shadcnexamples.com/blog-detail-page  template */}
-            <PostHeader
-              category={post.tags[0]}
-              dateField={post.data.published_date}
-              title={post.data.title}
-            />
+    <div className="container mx-auto bg-background min-h-screen lg:px-6">
+      {/* https://shadcnexamples.com/blog-detail-page  template */}
+      <PostHeader
+        category={post.tags[0]}
+        dateField={post.data.published_date}
+        title={post.data.title}
+      />
 
-            <div className="mt-6">
-              <Content
-                content={post.data.content}
-                coverImage={post.data.featured_image}
-              />
-            </div>
-          </div>
-        </div>
+      <div className="mt-6">
+        <Content
+          content={post.data.content}
+          coverImage={post.data.featured_image}
+        />
       </div>
     </div>
   );
