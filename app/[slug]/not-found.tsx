3e202d22de880780314811a3c0ebc69849188337
akhill7 <akhil.ashokan@litmus7.com>
2026-03-10 18:55:31 +0530

    refactor: add errorMessage prop

--- app/[slug]/not-found.tsx
+++ app/[slug]/not-found.tsx
@@ -1,14 +1,18 @@
 import LinkWithBenefits from '@/components/linkWithBenefits'
 import { Button } from '@/components/ui/button'
 
-export default function NotFound() {
+type NotFound = {
+    errorMessage?: string;
+}
+
+export default function NotFound({ errorMessage = "We couldn't find what you're looking for." }: NotFound) {
     return (
         <div className='flex justify-center items-center text-center h-[80vh]'>
             <div className='text-muted-foreground text-xl space-y-4'>
                 <h1 className='text-8xl'>oops...</h1>
-                <p className='text-lg'>We couldn't find what you're looking for.</p>
+                <p className='text-lg'>{errorMessage}</p>
                 <LinkWithBenefits href="/" className='-ml-4'>
-                    <Button variant={'outline'} className='rounded-full'>
+                    <Button variant={'outline'} className='rounded-full cursor-pointer'>
                         return home
                     </Button>
                 </LinkWithBenefits>
