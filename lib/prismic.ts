1c5c17d41157d2e92a7b7071bd0e90d3b586102e
akhill7 <akhil.ashokan@litmus7.com>
2026-03-11 19:44:54 +0530

    chore: remove unused comments

--- lib/prismic.ts
+++ lib/prismic.ts
@@ -1,7 +1,6 @@
 import * as prismic from '@prismicio/client';
 
 const repoName = process.env.NEXT_PUBLIC_REPO_NAME || ''
-// const endpoint = `https://${repoName}.prismic.io/api/v2`;
 const client = prismic.createClient(repoName);
 
 export default client;
