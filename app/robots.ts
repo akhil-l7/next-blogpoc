5b8b0b8f65b15c8a06cfcfbf462c9b960c73369c
akhill7 <akhil.ashokan@litmus7.com>
2026-03-11 19:13:11 +0530

    refactor: add sitemap, add disallow

--- app/robots.ts
+++ app/robots.ts
@@ -1,10 +1,15 @@
 import type { MetadataRoute } from 'next'
+import { config } from "@/app.config";
 
 export default function robots(): MetadataRoute.Robots {
+    const baseUrl = `https://${config.url}`;
+    
     return {
         rules: {
             userAgent: '*',
             allow: '/',
+            disallow: ['/api/'],
         },
+        sitemap: `${baseUrl}/sitemap.xml`,
     }
 }
