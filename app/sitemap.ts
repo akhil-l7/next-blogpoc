56550385c01ab4a1c6297bca28d2b7333baa298f
akhill7 <akhil.ashokan@litmus7.com>
2026-03-11 17:45:29 +0530

    add init sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,28 @@
+import { MetadataRoute } from 'next';
+import client from '@/lib/prismic';
+import { config } from '@/app.config';
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const baseUrl = `https://${config.url}`;
+  
+  const posts = await client.getAllByType('blog');
+  
+  const blogPosts = posts.map((post) => ({
+    url: `${baseUrl}/${post.id}`,
+    lastModified: post.last_publication_date ? new Date(post.last_publication_date) : new Date(),
+    changeFrequency: 'weekly' as const,
+    priority: 0.8,
+  }));
+
+  const staticPages: MetadataRoute.Sitemap = [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+  ];
+
+  return [...staticPages, ...blogPosts];
+}
+
