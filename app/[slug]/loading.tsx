c49018ed6fd7ca6c08e32b46ef4afdb6b3348e59
akhill7 <akhil.ashokan@litmus7.com>
2026-04-01 16:15:29 +0530

    refactor: add ViewTransition to Skeleton

--- app/[slug]/loading.tsx
+++ app/[slug]/loading.tsx
@@ -1,25 +1,28 @@
 import { Item, ItemContent, ItemHeader, ItemMedia } from "@/components/ui/item";
 import { Separator } from "@/components/ui/separator";
 import { Skeleton } from "@/components/ui/skeleton";
+import { ViewTransition } from "react";
 
 export default function Loading() {
   return (
     <div className="container mx-auto bg-background min-h-screen lg:px-6">
       {/* Post Header Skeleton */}
-      <div className="space-y-10 animate-pulse">
-        <div className="flex flex-col gap-2">
-          <div className="flex flex-wrap items-center gap-4 h-6">
-            <Skeleton className="h-8 w-20" />
-            <Skeleton className="h-4 w-24" />
+      <ViewTransition>
+        <div className="space-y-10 animate-pulse">
+          <div className="flex flex-col gap-2">
+            <div className="flex flex-wrap items-center gap-4 h-6">
+              <Skeleton className="h-8 w-20" />
+              <Skeleton className="h-4 w-24" />
+            </div>
+            <Separator />
           </div>
-          <Separator />
-        </div>
 
-        <div>
-          <Skeleton className="h-12 w-3/4 mb-4" />
-          <Skeleton className="h-8 w-1/2" />
+          <div>
+            <Skeleton className="h-12 w-3/4 mb-4" />
+            <Skeleton className="h-8 w-1/2" />
+          </div>
         </div>
-      </div>
+      </ViewTransition>
 
       {/* Content Skeleton */}
       <div className="mt-6 space-y-4">
@@ -42,12 +45,12 @@ export default function Loading() {
           </ItemContent>
         </ItemHeader>
         <Separator />
-        
+
         <div className="space-y-4 w-full my-4">
           <Skeleton className="h-24 w-full" />
           <Skeleton className="h-24 w-full" />
         </div>
-        
+
         <Skeleton className="h-32 w-full" />
       </Item>
     </div>
